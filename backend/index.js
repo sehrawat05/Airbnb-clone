90018798e3b77be2e46be80365444faca222433f
sehrawat05 <135001791+sehrawat05@users.noreply.github.com>
2026-04-12 16:38:37 +0530

    Change CORS origin to localhost for development

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ let app=express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://airbnb-xcgw.onrender.com",
+    origin: "http://localhost:5173",
     credentials: true,
 }))
 
